91b17741138a123781fede375d2ec861aa4dcaeb
Jorge Oney <guinodo@gmail.com>
2021-12-11 23:37:39 -0300

    Create domains

---
+++ src/main/java/com/guinodo/pismo/transaction/application/domain/Account.java
@@ -0,0 +1,22 @@
+package com.guinodo.pismo.transaction.application.domain;
+
+public class Account {
+    private Long accountId;
+    private String documentNumber;
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+}
---
+++ src/main/java/com/guinodo/pismo/transaction/application/domain/Transaction.java
@@ -0,0 +1,42 @@
+package com.guinodo.pismo.transaction.application.domain;
+
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private Long transactionId;
+    private Account account;
+    private Double amount;
+    private LocalDateTime eventDate;
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDateTime eventDate) {
+        this.eventDate = eventDate;
+    }
+}
