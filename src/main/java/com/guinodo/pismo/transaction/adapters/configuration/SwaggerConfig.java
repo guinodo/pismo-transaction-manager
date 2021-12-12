350d302b0c06f9b10a2d985ecf8684c961050ca1
Jorge Oney <guinodo@gmail.com>
2021-12-11 23:38:36 -0300

    Create documentations

---
+++ src/main/java/com/guinodo/pismo/transaction/adapters/configuration/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.guinodo.pismo.transaction.adapters.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket configApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.guinodo.pismo.transaction.adapters.inbound.controllers"))
+                .paths(PathSelectors.any())
+                .build()
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class)
+                .useDefaultResponseMessages(true)
+                .enableUrlTemplating(true);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Backend app Transaction Manager")
+                .description("Transactions control app")
+                .contact(new Contact("Jorge Teixeira", "https://github.com/guinodo", "guinodo@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0")
+                .build();
+    }
+
+}
